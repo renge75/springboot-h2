4056f9252f8e55a5e0e6046166aad2ecfd5028dc
renge75 <1005875478@qq.com>
2020-03-17 22:04:28 +0800

    remove a line

--- src/main/java/ren/shuaibing/sql/AppStart.java
+++ src/main/java/ren/shuaibing/sql/AppStart.java
@@ -7,7 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class AppStart {
     
     public static void main(String[] args) {
-   
         SpringApplication.run(AppStart.class, args);
     }
 
