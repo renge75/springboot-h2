5ff99c9b111b2ee9a723c25518064d883a290bac
renge75 <1005875478@qq.com>
2020-03-19 21:44:56 +0800

    test api

--- src/test/java/ren/shuaibing/sql/FishApiTest.java
+++ src/test/java/ren/shuaibing/sql/FishApiTest.java
@@ -6,25 +6,32 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestMethodOrder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.reactive.server.WebTestClient;
+import org.springframework.test.web.servlet.MockMvc;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+/**
+ * 测试接口mvc
+ */
 @SpringBootTest
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
-@AutoConfigureWebTestClient
+
+@AutoConfigureMockMvc
 class FishApiTest {
 
-    @Autowired WebTestClient webClient;
+
 
     @Test
     @Order(2)
-    void fishList() {
-        webClient.get().uri("/fish/list").exchange().expectStatus().isOk()
-                //.expectBody(String.class).isEqualTo("Hello World")
-        ;
-
+    void fishList(@Autowired MockMvc mvc) throws Exception{
+        mvc.perform(get("/fish/list")).andExpect(status().is(200));
     }
 
     @Test
